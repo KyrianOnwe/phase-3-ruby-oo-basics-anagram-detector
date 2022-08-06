f4fd07dfbff8aa7afb63a76adcbdc3afb0b3eed0
Kyrian <dr.kyrian.onwe@gmail.com>
2022-08-05 20:27:40 -0500

    commit tree 863f52cf88e9b42c7c39add99d8f98620cc966b9

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+
+
+class Anagram
+    def initialize(word)
+        @word = word        
+    end
+    attr_accessor :word
+
+    
+    def match(array)
+       ans = array.filter{|arr| arr.length == @word.length}
+       result = ans.filter{|f| f.chars.sort == @word.chars.sort}
+       if (!result)
+        []
+       else
+        result
+       end
+    end
+end
